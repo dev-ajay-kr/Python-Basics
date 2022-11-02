ec1777e1bc19dc847c25e796048733de8a9b6ad2
dev-ajay-kr <117085146+dev-ajay-kr@users.noreply.github.com>
2022-11-02 10:17:57 +0530

    done till zoo kepper

---
+++ Python Basics/min2Num.py
@@ -0,0 +1,18 @@
+# def minimum(a, b):
+#     if a <= b:
+#         return a
+#     else:
+#         return b
+#
+
+# Driver code
+# a = 2
+# b = 4
+# print(minimum(a, b))
+i = 0
+a = 'a'
+while i < 8:
+    a *= 2
+    i += 1
+print(a)
+print(len(a))
