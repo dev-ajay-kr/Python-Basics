e824b928514996b41510517b2e09536727c79d7a
dev-ajay-kr <117085146+dev-ajay-kr@users.noreply.github.com>
2022-11-03 12:12:46 +0530

    Added number methods

---
+++ Python Basics/Number-Math Function.py
@@ -0,0 +1,33 @@
+# This will import math module
+import math
+
+
+
+#
+#
+print("math.ceil(-23.11)", math.ceil(-23.11))
+print("math.floor(-23.11) : ", math.floor(-23.11))
+print("math.floor(300.72) : ", math.floor(300.72))
+print("abstract of 98: ", abs(98))
+print("ceil(5.9)", math.ceil(5.9))
+# print("cmp(x, y)-1 if x: < y, 0 if x == y, or 1 if x > y", cmp(6,5))
+print("exp()x", math.exp(100))
+print("	ceil(x)", math.fabs(7))
+print("pow (2,2)", math.pow(2,2))
+math.sqrt(144)
+
+'''### other imp functiion
+	Function and Description
+1	choice(seq)
+A random item from a list, tuple, or string.
+2	randrange ([start,] stop [,step])
+A randomly selected element from range(start, stop, step)
+3	random()
+A random float r, such that 0 is less than or equal to r and r is less than 1
+4	seed([x])
+Sets the integer starting value used in generating random numbers. Call this function before calling any other random module function. Returns None.
+5	shuffle(lst)
+Randomizes the items in a list without moving them. Returns None.
+6	uniform(x, y)
+A random float r, such that x is less than or equal to r and r is less than y
+'''
