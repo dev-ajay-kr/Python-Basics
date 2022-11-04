4d8e8754e318219ff078aa19e9dd6defa8ea0733
dev-ajay-kr <117085146+dev-ajay-kr@users.noreply.github.com>
2022-11-04 13:21:23 +0530

    exception

---
+++ Python Advanced/Exeption.py
@@ -0,0 +1,46 @@
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+
+
+
+
+# when there is no exception else can be useed
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+
+#finally
+
+
+
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+
+
+
+
+
+
+#example
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
+
