907002b4aea71dfec9b1568f2250807f41e2f715
dev-ajay-kr <117085146+dev-ajay-kr@users.noreply.github.com>
2022-11-05 15:39:56 +0530

    added line, markers, plotting and rainbow on a graph

---
+++ Python Advanced/Matplot/line.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted', c='hotpink')
+plt.show()
+
+
+# #
+# Shorter Syntax
+# The line style can be written in a shorter syntax:
+#
+# linestyle can be written as ls.
+#
+# dotted can be written as :.
+#
+# dashed can be written as --.
+#
+# Line Styles
+# You can choose any of these styles:
+#
+# Style	Or
+# 'solid' (default)	'-'
+# 'dotted'	':'
+# 'dashed'	'--'
+# 'dashdot'	'-.'
+# 'None'	'' or ' '
